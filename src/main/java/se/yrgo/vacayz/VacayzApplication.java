e3c8999a0aaa17f1f320a2feae73a3f1836934d5
TomasYrgo <150817213+TomasYrgo@users.noreply.github.com>
2024-05-22 13:50:12 +0200

    springinitializr

---
+++ src/main/java/se/yrgo/vacayz/VacayzApplication.java
@@ -0,0 +1,13 @@
+package se.yrgo.vacayz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacayzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacayzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/yrgo/vacayz/VacayzApplicationTests.java
@@ -0,0 +1,13 @@
+package se.yrgo.vacayz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacayzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
