72db5e55bb92c303d0ace7ad558f5153d5b675e0
Linnea <150425979+linwox@users.noreply.github.com>
2024-05-23 13:17:09 +0200

    new domain class House

---
+++ src/main/java/se/yrgo/domain/House.java
@@ -0,0 +1,84 @@
+package se.yrgo.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class House {
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    int id;
+    String objectId;
+    String address;
+    String area;
+    String location;
+    int maxGuests;
+    boolean available;
+    String imageUrl;
+
+    public House() {}
+
+    public String getObjectId() {
+        return objectId;
+    }
+
+    public void setObjectId(String objectId) {
+        this.objectId = objectId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getMaxGuests() {
+        return maxGuests;
+    }
+
+    public void setMaxGuests(int maxGuests) {
+        this.maxGuests = maxGuests;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("House info: %d, %s, %s, %s, %s, %d, Available: %b, image: %s",
+                id, objectId, address, area, location, maxGuests, available, imageUrl);
+    }
+}
